39df217e4f4d498ff52c5a9ed09a757d310dbfab
Lucas Wilhelmsen <lucaswilhelmsen@Lucass-MacBook-Pro.local>
2026-01-04 20:18:01 +0100

    preparation for pages

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, session, render_template, redirect
 import flask_sqlalchemy
 import os 
 
-app = Flask(__name__)
+app = Flask(__name__,
+template_folder="docs")
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
 @app.route("/")
