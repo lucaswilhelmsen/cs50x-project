1ec64a22f0137c950bd12f75175e6a27cd25d737
Lucas Wilhelmsen <lucaswilhelmsen@Lucass-MacBook-Pro.local>
2026-01-04 17:03:03 +0100

    update controller

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, session, render_template, redirect
+import flask_sqlalchemy
+import os 
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/writing")
+def writing():
+    return render_template("writing.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/pictures")
+def pictures():
+    return render_template("pictures.html")
