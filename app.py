af72e2b010926b334335d8af6da0949aa5a6613f
Lucas Wilhelmsen <lucaswilhelmsen@Lucass-MacBook-Pro.local>
2026-01-10 17:21:55 +0100

    updated files

--- app.py
+++ app.py
@@ -14,11 +14,10 @@ engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 def index():
     return render_template("index.html")
 
-@app.route("/first_project")
-def first_project():
-    if request.method == "GET":
-        result = engine.execute("SELECT * FROM projects")
-        return render_template("first_project.html", projects=result)
+@app.route("/projects")
+def projects():
+    result = engine.execute("SELECT * FROM projects")
+    return render_template("projects.html", projects=result)
 
 
 @app.route("/writing")
